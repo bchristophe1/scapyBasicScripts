bc1d0a63bcfe8edad731c2fa3b98ea76e537f6f1
John Doe <contact@benjamin-christophe.fr>
2016-08-06 14:37:35 +0200

    Update Frame Params of CreatePackets.py

--- CreatePackets.py
+++ CreatePackets.py
@@ -6,5 +6,10 @@ print("\nThis Script will create an Ethernet Frame")
 
 # Creation of Ethernet Frame with scapy defaults parameters
 MyFrame = Ether()
+
+# Definition of Frame parameters
+MyFrame.dst = '00:AA:BB:CC:DD:EE'
+MyFrame.src = '01:0A:0B:0C:0D:0E'
+
 # Call show scapy methods for see attributs of this Ethernet Frame (dst,src,type)
 MyFrame.show()
